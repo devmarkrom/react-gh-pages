1a4b2c263d3c58ab9873b95913d8d5a3524c8c87
Mark <marcosromeromtz@gmail.com>
2018-10-14 20:34:38 -0500

    Updates

---
+++ precache-manifest.7842385cfb0d209f087997a236d1be0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "f33ab39c33e6310f2954",
+    "url": "/react-gh-pages/static/js/main.f33ab39c.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/react-gh-pages/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "f33ab39c33e6310f2954",
+    "url": "/react-gh-pages/static/css/main.8079a2b4.chunk.css"
+  },
+  {
+    "revision": "f8388415341262a0df5e333d7ae11cd9",
+    "url": "/react-gh-pages/index.html"
+  }
+];
